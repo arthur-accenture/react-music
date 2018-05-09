aa39d64da1ba0dd5cb3960d6d45b6183df01e33a
Arthur Cheung <arthur.cheung@accenture.com>
2018-05-09 14:40:16 +1000

    Used envelope to set ASDR settings on melody.

--- demo/parts/melody.js
+++ demo/parts/melody.js
@@ -1,31 +1,35 @@
 import React from 'react';
 
-import { Monosynth, Delay, Reverb } from '../../src';
+import { Monosynth, LFO } from '../../src';
 
 export const Melody = (props) => (
-    <Delay>
-        <Reverb>
-            <Monosynth
-                gain={0.3}
-                glide={0.05}
-                type="triangle"
-                steps={[
-                    // F major
-                    [3, 4, 'g4'],
-                    [6, 4, 'e4'],
-                    // C Major
-                    [11, 4, 'd4'],
-                    [14, 4, 'c4'],
-                    //   // G major
-                    [19, 4, 'e4'],
-                    [22, 4, 'g4'],
-                    //     // A minor
-                    [27, 4, 'd4'],
-                    [30, 8, 'a3'],
-                ]}
-            />
-         </Reverb>
-     </Delay>
+    <Monosynth
+        envelope={{
+            attack: 3,
+            sustain: 5,
+            decay: 2,
+            release: 0.1,
+        }}
+        gain={0.3}
+        glide={0.05}
+        type="triangle"
+        steps={[
+            // F major
+            [3, 4, 'g4'],
+            [6, 4, 'e4'],
+            // C Major
+            [11, 4, 'd4'],
+            [14, 4, 'c4'],
+            //   // G major
+            [19, 4, 'e4'],
+            [22, 4, 'g4'],
+            //     // A minor
+            [27, 4, 'd4'],
+            [30, 8, 'a3'],
+        ]}
+    >
+        {/* <LFO type="sine" frequency={0.5} connect={c => c.detune}/> */}
+    </Monosynth>
 );
 
 export default Melody;
