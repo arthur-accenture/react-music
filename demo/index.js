9d61b72a91cc19f6c3440cc243cbdc8480838832
Arthur Cheung <arthur.cheung@accenture.com>
2018-05-09 19:28:04 +1000

    Made hotloading null safe so that it doesn't cause issues in production.

--- demo/index.js
+++ demo/index.js
@@ -10,12 +10,17 @@ ReactDOM.render(
   document.getElementById('root')
 );
 
-module.hot.accept('./demo', () => {
-  const NextDemo = require('./demo').default;
-  ReactDOM.render(
-    <AppContainer>
-      <NextDemo />
-    </AppContainer>,
-    document.getElementById('root')
-  );
-});
+if(module.hot){
+  // This code only runs if hot loading module is present
+  // Ideally we should probably exclude this code from production
+  module.hot.accept('./demo', () => {
+    const NextDemo = require('./demo').default;
+    ReactDOM.render(
+      <AppContainer>
+        <NextDemo />
+      </AppContainer>,
+      document.getElementById('root')
+    );
+  });
+}
+
