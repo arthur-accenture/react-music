8ab7bb84a7ee200b130e6957d5cf782ba7a7fc69
Arthur Cheung <arthur.cheung@accenture.com>
2018-05-09 11:54:14 +1000

    Added reverb and delay to melody.

--- demo/parts/melody.js
+++ demo/parts/melody.js
@@ -1,27 +1,31 @@
 import React from 'react';
 
-import { Monosynth } from '../../src';
+import { Monosynth, Delay, Reverb } from '../../src';
 
 export const Melody = (props) => (
-    <Monosynth
-        gain={0.3}
-        glide={0.05}
-        type="triangle"
-        steps={[
-            // F major
-            [3, 4, 'g4'],
-            [6, 4, 'e4'],
-            // C Major
-            [11, 4, 'd4'],
-            [14, 4, 'c4'],
-            //   // G major
-            [19, 4, 'e4'],
-            [22, 4, 'g4'],
-            //     // A minor
-            [27, 4, 'd4'],
-            [30, 6, 'a3'],
-        ]}
-    />
+    <Delay>
+        <Reverb>
+            <Monosynth
+                gain={0.3}
+                glide={0.05}
+                type="triangle"
+                steps={[
+                    // F major
+                    [3, 4, 'g4'],
+                    [6, 4, 'e4'],
+                    // C Major
+                    [11, 4, 'd4'],
+                    [14, 4, 'c4'],
+                    //   // G major
+                    [19, 4, 'e4'],
+                    [22, 4, 'g4'],
+                    //     // A minor
+                    [27, 4, 'd4'],
+                    [30, 8, 'a3'],
+                ]}
+            />
+         </Reverb>
+     </Delay>
 );
 
 export default Melody;
