356a8346f320ee497354b2310a155c281f69948b
Arthur Cheung <arthur.cheung@accenture.com>
2018-05-09 11:45:16 +1000

    Changed melody to use Monosynth to incorporate glides.

--- demo/parts/melody.js
+++ demo/parts/melody.js
@@ -1,24 +1,25 @@
 import React from 'react';
 
-import { Synth } from '../../src';
+import { Monosynth } from '../../src';
 
 export const Melody = (props) => (
-    <Synth
+    <Monosynth
         gain={0.3}
+        glide={0.05}
         type="triangle"
         steps={[
             // F major
-            [3, 4, ['g4']],
-            [6, 2, ['e4']],
+            [3, 4, 'g4'],
+            [6, 4, 'e4'],
             // C Major
-            [11, 4, ['d4']],
-            [14, 2, ['c4']],
+            [11, 4, 'd4'],
+            [14, 4, 'c4'],
             //   // G major
-            [19, 4, ['e4']],
-            [22, 2, ['g4']],
+            [19, 4, 'e4'],
+            [22, 4, 'g4'],
             //     // A minor
-            [27, 4, ['d4']],
-            [30, 2, ['a3']],
+            [27, 4, 'd4'],
+            [30, 6, 'a3'],
         ]}
     />
 );
