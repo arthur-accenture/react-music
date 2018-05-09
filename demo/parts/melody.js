d7d505675d363d2e296c2b185df9620f6dd0ce7c
Arthur Cheung <arthur.cheung@accenture.com>
2018-05-09 15:32:27 +1000

    Updated ADSR to represent more of a stringed staccato feel.

--- demo/parts/melody.js
+++ demo/parts/melody.js
@@ -5,30 +5,30 @@ import { Monosynth, LFO } from '../../src';
 export const Melody = (props) => (
     <Monosynth
         envelope={{
-            attack: 3,
-            sustain: 5,
-            decay: 2,
+            attack: 0.01,
+            decay: 0.1,
+            sustain: 0.1,
             release: 0.1,
         }}
-        gain={0.3}
-        glide={0.05}
-        type="triangle"
+        gain={0.6}
+        glide={0.01}
+        type="sine"
         steps={[
             // F major
             [3, 4, 'g4'],
-            [6, 4, 'e4'],
+            [6, 2, 'e4'],
             // C Major
             [11, 4, 'd4'],
-            [14, 4, 'c4'],
+            [14, 2, 'c4'],
             //   // G major
             [19, 4, 'e4'],
-            [22, 4, 'g4'],
+            [22, 2, 'g4'],
             //     // A minor
             [27, 4, 'd4'],
-            [30, 8, 'a3'],
+            [30, 2, 'a3'],
         ]}
     >
-        {/* <LFO type="sine" frequency={0.5} connect={c => c.detune}/> */}
+
     </Monosynth>
 );
 
