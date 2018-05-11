401f469d0189c85766763b85b09cc68cb7de2268
Arthur Cheung <arthur.cheung@accenture.com>
2018-05-11 11:03:38 +1000

    Changed reference to samples to absolute path to make work with dev and prod version.

--- demo/parts/beats.js
+++ demo/parts/beats.js
@@ -8,12 +8,12 @@ export const Beats = (props) => (
     bars={1}
   >
     <Sampler
-      sample="../samples/kick.wav"
+      sample="/samples/kick.wav"
       steps={[0, 2]}
       detune={-1000}
     />
     {/* <Sampler
-      sample="../samples/cowbell.wav"
+      sample="/samples/cowbell.wav"
       steps={[2]}
       detune={5000}
     /> */}
